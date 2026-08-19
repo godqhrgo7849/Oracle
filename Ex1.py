aa0d376223c0d9f2b3f354f626d93dbabff431e0
godqhrgo7849 <63216333+godqhrgo7849@users.noreply.github.com>
2026-08-19 13:44:44 +0900

    Add files via upload
    직업훈련 1일차 python 기본기

---
+++ Ex1.py
@@ -0,0 +1,40 @@
+# Day 2026-08-19
+
+# 출력문 print(데이터(literal)계산식/변수,수식,함수,end="\n|문자열")
+
+print("Hello, Python!")
+print(2026)
+print(3.14)
+print(10+10)
+print("Life is"+"too short")
+
+# 이스케이프 문자 
+
+'''
+\n : 줄 바꿈
+\t : 탭
+\\ : 백슬래시
+\' : 작은따옴표
+\" : 큰따옴표
+'''
+txt1 = "김철수"
+txt2 = "고길동"
+txt3 = "박지민"
+print(txt1)
+print('\n\t\t',txt2, '\n\n')
+print(txt3)
+
+# 변수 - 어떤 값을 저장하는 메모리 공간
+
+''' 3 + 5 + 6 = ?
+    계산식 문자열, 계산식
+    print("3 + 5 + 6 =",3 + 5 + 6)
+    print(a,b,c)
+    a,b,c = 100,200,300
+'''
+n1 = 3
+n2 = 5
+n3 = 6
+sum = n1+n2+n3
+print(n1,"+",n2,"+",n3,"=",sum)
+
